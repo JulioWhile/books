ec978b9f34c05b902b5f10c1d160b9f333a1aa89
Erick De Santiago <erick.desantiago@wizeline.com>
2020-11-25 12:29:27 -0600

    feat: Added routing with switch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Container from "./App.styled";
 import AuthProvider from "./providers/Auth";
 import NavBar from "./components/NavBar";
 import Books from "./pages/books";
+import Login from "./pages/login";
+import Home from "./pages/home";
+import BookDetails from "./components/BookDetails";
 
 function App() {
   return (
@@ -12,7 +15,12 @@ function App() {
       <BrowserRouter>
         <Container>
           <NavBar />
-          <Books />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/book/:id" component={BookDetails} />
+            <Route path="/books" component={Books} />
+          </Switch>
         </Container>
       </BrowserRouter>
     </AuthProvider>
